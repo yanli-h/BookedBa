a204928e7768abd2dbcad0e32957f44febc6c724
👄 Yanli <u04xu4y@gmali.com>
2022-09-05 00:54:57 +0800

    init first

---
+++ src/main/java/com/BookedBaApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookedBaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookedBaApplication.class, args);
+	}
+
+}
